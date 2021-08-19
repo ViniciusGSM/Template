3e796521c503c57db7f6b8017e463ee1f1a13b36
ViniciusGSM <89159174+ViniciusGSM@users.noreply.github.com>
2021-08-19 16:42:29 -0300

    Instalando e configurando EF Core em um projeto .Net Core

---
+++ Template.Data/Context/TemplateContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Data.Extensions;
+using Template.Data.Mappings;
+using Template.Domain.Entities;
+
+namespace Template.Data.Context
+{
+    //classe de contexto
+    public class TemplateContext: DbContext
+    {
+        //Constructor//
+        public TemplateContext(DbContextOptions<TemplateContext> option) 
+            : base(option) { }
+
+
+        //Por n ter classe de mapeamento, foi criado esse dbset dentro dessa regiao:
+        #region "DBSets"
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+
+        //Precisa setar aqui a classe de mapeamento para pegar todas as configuracoes na hora de rodar:
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            modelBuilder.SeedData();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }// agora precisa setar na startup no projeto web pra rodar na aplicacao e criar uma conexao na appsettings.jason
+
+
+}
---
+++ Template.Data/Mappings/UserMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Domain.Entities;
+
+namespace Template.Data.Mappings
+{
+    /* NuGet: EntityFrameworkCore, SqlServer, SqlServerDesign
+    
+    Classe de Mapeamento do usuario */
+    public class UserMap: IEntityTypeConfiguration<User> //interface da entityframework core com a entidade que vc precisa, no caso User
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            //aqui dentro voce estipula as configuracoes desejadas
+
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+        }
+            
+    }
+}
