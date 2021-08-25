96d7025cd5595c6b2458ba02136560018eaf7415
ViniciusGSM <89159174+ViniciusGSM@users.noreply.github.com>
2021-08-25 18:21:52 -0300

    Configuração Swagger

--- Template/Startup.cs
+++ Template/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Template.Application.AutoMapper;
 using Template.Data.Context;
 using Template.IoC;
+using Template.Swagger;
 
 namespace Template
 {
@@ -32,6 +33,7 @@ namespace Template
             NativeInjector.RegisterServices(services);
 
             services.AddAutoMapper(typeof(AutoMapperSetup));
+            services.AddSwaggerConfiguration();
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -54,6 +56,8 @@ namespace Template
                 app.UseHsts();
             }
 
+
+            app.UseSwaggerConfiguration();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
